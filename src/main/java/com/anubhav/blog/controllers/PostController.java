8d3286d56b0dc108657f44d8c6fd0cd917cecaec
Anubhav Bej <bej.anubhav@gmail.com>
2022-12-25 20:47:11 +0530

    add Constant file for project

--- src/main/java/com/anubhav/blog/controllers/PostController.java
+++ src/main/java/com/anubhav/blog/controllers/PostController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.anubhav.blog.config.AppConstants;
 import com.anubhav.blog.payloads.ApiResponse;
 import com.anubhav.blog.payloads.PageResponse;
 import com.anubhav.blog.payloads.PostDto;
@@ -94,10 +95,10 @@ public class PostController {
 //	pagicnation
 	@GetMapping("/posts/pagicnationResponse")
 	public ResponseEntity<PageResponse> getAllPostsWithResponse(
-			@RequestParam(value = "pageNumber", defaultValue = "0",required = false) Integer pageNumber,
-			@RequestParam(value = "pageSize", defaultValue = "5",required = false) Integer pageSize,
-			@RequestParam(value = "sortBy", defaultValue = "title",required = false) String sortBy,
-			@RequestParam(value = "sortDir", defaultValue = "asc",required = false) String sortDir
+			@RequestParam(value = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER,required = false) Integer pageNumber,
+			@RequestParam(value = "pageSize", defaultValue = AppConstants.PAGE_SIZE,required = false) Integer pageSize,
+			@RequestParam(value = "sortBy", defaultValue = AppConstants.SORT_BY,required = false) String sortBy,
+			@RequestParam(value = "sortDir", defaultValue = AppConstants.SORT_DIR,required = false) String sortDir
 
 			){
 		PageResponse postPagicnationWithResponse = postServiceImpl.postPagicnationWithResponse(pageNumber, pageSize, sortBy, sortDir);
