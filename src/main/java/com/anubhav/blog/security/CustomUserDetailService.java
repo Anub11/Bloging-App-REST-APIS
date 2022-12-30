52bee1e015eb038d38728535c264922c42d5efe9
Anubhav Bej <bej.anubhav@gmail.com>
2022-12-30 22:20:47 +0530

    Added Spring Security

---
+++ src/main/java/com/anubhav/blog/security/CustomUserDetailService.java
@@ -0,0 +1,30 @@
+package com.anubhav.blog.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.anubhav.blog.entities.User;
+import com.anubhav.blog.exceptions.ResourceNotFoundException;
+import com.anubhav.blog.repositories.UserRepo;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService{
+
+	@Autowired
+	private UserRepo userRepo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		User user = this.userRepo.findByEmail(username).orElseThrow(()->new ResourceNotFoundException("username", "not valid"+username, 0));
+
+		
+		return user;
+	}
+	
+	
+
+}
